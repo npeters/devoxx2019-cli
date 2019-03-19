beaa54a5ffa9a16802293e24d8fe51d81c02ab85
Nicolas Peters <nicolas.peters@canal-plus.com>
2019-03-19 21:45:49 +0100

    init commit

---
+++ demos/jaotc/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class HelloWorld {
+    static {
+        System.out.println(new Date());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Jaotc");
+    }
+}
